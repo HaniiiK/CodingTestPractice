7f277c819d11465f2b49b1fd5a9c7ac993f39506
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-03-22 12:15:28 +0900

    [level 0] Title: 자릿수 더하기, Time: 8.40 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = n+"";
+        char[] c = new char[str.length()];
+        int[] num = new int[str.length()];
+        
+        for(int i=0; i<str.length(); i++) {
+            c[i] = str.charAt(i);
+            num[i] = c[i] - '0';
+        }
+        
+        for(int i=0; i<num.length; i++) {
+            answer += num[i];
+        }
+        
+        return answer;
+    }
+}
