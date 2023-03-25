c262b784fbbf7e3883a6b92e575624f943cccddf
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-03-25 15:36:05 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -5,19 +5,18 @@ class Solution {
         
         int cnt=1;
         for(int i=0; i<=len+1; i+=2) {
-            if(i == len) {
+            if(i == len) { //배열의 뒤에서 두번째걸 가르키고 있으면 0번 인덱스로
                 i=0;
-            } else if(i == len+1) {
+            } else if(i == len+1) { //배열의 제일 마지막걸 가르키고 있으면 1번 인덱스로
                 i=1;
             }
-            if(cnt==k) {
+            if(cnt==k) { //k번째가 되면 return
                 answer = numbers[i];
                 break;
-            } else {
+            } else { 
                 cnt++;                
             }
         }
-        
         return answer;
     }
 }
