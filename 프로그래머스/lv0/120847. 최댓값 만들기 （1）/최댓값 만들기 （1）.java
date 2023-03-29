4c1c01b7a8036642fda0d84f432e7ee160687b2d
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-03-29 13:22:48 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.46 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        int m1=0, m2=0, idx=0;
+        int len = numbers.length;
+        
+        Arrays.sort(numbers);
+        answer = numbers[len-1] * numbers[len-2];
+        
+/*        int m1=0, m2=0, idx=0;
         for(int i=0; i<numbers.length; i++) {
             if(m1<numbers[i]) {
                 m1 = numbers[i];
@@ -16,7 +23,7 @@ class Solution {
                 m2 = numbers[i];
             }
         }
-        answer = m1*m2;
+        answer = m1*m2;*/
         return answer;
     }
 }
