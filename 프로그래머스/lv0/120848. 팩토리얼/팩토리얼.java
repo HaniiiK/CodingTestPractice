bc300de3b4e26ff6850fd430704178e4ea3553c2
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-03-30 21:43:48 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,7 +1,28 @@
 class Solution {
-    public int solution(int n) {        
+    public int solution(int n) {    
         int answer = 0;
-/*        int i=1, res=n;
+        int factorial = 1;
+int index = 1;
+while(factorial <= n){
+    factorial *= index;
+    
+    if(factorial <= n){
+         index++;
+     }else if(factorial == n){
+         answer = index;
+     }else if(n < factorial){
+        answer = (index-1);
+    } 
+    if(factorial == 0){
+        answer = 0;
+        return answer;
+    }
+}
+return answer;
+        
+        
+    /*    int answer = 0;
+        int i=1, res=n;
         while(true) {
             res = res/i;
             if(res>i) {
@@ -12,24 +33,5 @@ class Solution {
             }
         }
         return answer;*/
-        int factorial = 1;
-        int index = 1;
-        if(n==1) {
-            answer = 1;
-        }else {
-        while(factorial < n){
-            factorial *= index;
-            
-            if(factorial < n){
-                 index++;
-             }else if(factorial == n){
-                 answer = index;
-             }else if(n < factorial){
-                answer = (index-1);
-            } 
-        }
-            
-        }
-        return answer;
     }
 }
