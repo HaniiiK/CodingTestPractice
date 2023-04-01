cbc2c031e3c5a44747c0ca37b917d01bba690e7e
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-04-01 13:47:51 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,18 +1,14 @@
+import java.util.ArrayList;
+
 class Solution {
-    public int[] solution(int n, int[] numlist) {
-        int[] ans = new int[numlist.length];
-        int index=0;
+    public ArrayList solution(int n, int[] numlist) {
+        ArrayList answer = new ArrayList();
+        
         for(int i=0; i<numlist.length; i++) {
             if(numlist[i]%n==0) {
-                ans[index] = numlist[i];
-                index++;
+                answer.add(numlist[i]);
             }
         }
-        int[] answer = new int[index];
-        for(int i=0; i<index; i++) {
-            answer[i] = ans[i];
-        }
-    
         return answer;
     }
 }
