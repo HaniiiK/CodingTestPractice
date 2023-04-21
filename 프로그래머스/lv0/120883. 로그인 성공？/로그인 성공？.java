b16a85d6d8538325f5a5019664cf1d015443befb
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-04-21 18:01:25 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,16 +1,32 @@
+import java.util.Map;
+import java.util.HashMap;
+
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "fail";
         
+        Map<String, String> map = new HashMap<>();
         for(int i=0; i<db.length; i++) {
-            if(db[i][0].equals(id_pw[0])) {
-                if(db[i][1].equals(id_pw[1])) {
-                    answer = "login";
-                    break;
-                }
+            map.put(db[i][0], db[i][1]);
+        }
+        
+        if(map.containsKey(id_pw[0])) {
+            if(map.get(id_pw[0]).equals(id_pw[1])) {
+                answer = "login";
+            }else {
                 answer = "wrong pw";
             }
         }
+        
+        // for(int i=0; i<db.length; i++) {
+        //     if(db[i][0].equals(id_pw[0])) {
+        //         if(db[i][1].equals(id_pw[1])) {
+        //             answer = "login";
+        //             break;
+        //         }
+        //         answer = "wrong pw";
+        //     }
+        // }
         
         return answer;
     }
