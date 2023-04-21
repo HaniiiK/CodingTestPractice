a85cd3d97f5f085e8406edee26cbc0b05821ea85
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-04-21 18:10:49 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        if(common[0]-common[1] == common[1]-common[2]) {
+        if(common[0]-common[1] == common[1]-common[2]) { //등차수열
             answer = common[common.length-1] + (common[1]-common[0]);
-        } else if(common[0]/common[1] == common[1]/common[2]) {
+        } else if(common[0]/common[1] == common[1]/common[2]) { //등비수열
             answer = common[common.length-1] * (common[1]/common[0]);
         }
         return answer;
