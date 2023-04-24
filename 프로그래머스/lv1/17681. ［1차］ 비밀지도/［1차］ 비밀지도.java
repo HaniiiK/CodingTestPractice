d37239401dcab08f044db10046446eeaea1c4166
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-04-24 16:10:45 +0900

    [level 1] Title: [1차] 비밀지도, Time: 16.56 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        String[] s1 = new String[n];
+        String[] s2 = new String[n];
+        int len = 0;
+        
+        for(int i=0; i<arr1.length; i++) {            //이진수 String 배열에 저장
+            s1[i] = Integer.toBinaryString(arr1[i])+"";
+            len = s1[i].length();
+            if(len<n) {
+                for(int j=0; j<n-len; j++) {
+                    s1[i] = "0".concat(s1[i]);
+                }
+            }
+            s2[i] = Integer.toBinaryString(arr2[i])+"";
+            len = s2[i].length();
+            if(len<n) {
+                for(int j=0; j<n-len; j++) {
+                    s2[i] = "0".concat(s2[i]);
+                }
+            }            
+
+            answer[i] = ""; //값 대입
+            for(int j=0; j<n; j++) {
+                if(s1[i].charAt(j)=='0' && s2[i].charAt(j)=='0') {
+                    answer[i] += " ";
+                }else {
+                    answer[i] += "#";
+                }                
+            }
+            
+        }
+        
+        return answer;
+    }
+}
