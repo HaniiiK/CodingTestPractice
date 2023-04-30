7f50cea18ad7a2a6bf6d9d44fbcffe7643ce2e26
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-04-30 21:51:03 +0900

    [level 2] Title: 숫자의 표현, Time: 1.68 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sum = 0;
+        int cnt = 0;
+        for(int i=1; i<=n; i++) {
+            sum += i;
+            cnt++;
+            if(sum==n) {
+                answer++;
+                sum = 0;
+                i = i-cnt+1;
+                cnt = 0;
+            }else if(sum>n) {
+                sum = 0;
+                i = i-cnt+1;
+                cnt = 0;                
+            }
+        }
+        return answer;
+    }
+}
