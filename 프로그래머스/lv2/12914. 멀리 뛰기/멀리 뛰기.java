f007ea6da44a41d871703bb55bd6aa8a63f4bbfb
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-04-30 22:44:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] arr = new long[n];
+        arr[0] = 1;
+        if(n!=1) {
+            arr[1] = 2;
+        }
+        
+        for(int i=2; i<n; i++) {
+            arr[i] = (arr[i-2] + arr[i-1])%1234567;
+        }
+        answer = arr[n-1];
+        return answer;
+    }
+}
