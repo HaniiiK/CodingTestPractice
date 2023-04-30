deb18cb255e53dbd47d0f158ba060cd8b400d6ce
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-04-30 11:34:57 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 11.24 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        for(int i=0; i<s.length(); i++) {
+            if(i==0 || (i!=0 && s.charAt(i-1)==' ')) {
+                answer += s.toUpperCase().charAt(i);                
+            } else {
+                answer += s.toLowerCase().charAt(i);
+            }
+        }
+        
+        
+        return answer;
+    }
+}
