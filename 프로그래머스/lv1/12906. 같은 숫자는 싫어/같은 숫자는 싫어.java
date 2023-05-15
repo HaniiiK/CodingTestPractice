eb5313130281ef625c2c758757c3d45cfb281e5b
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-05-15 19:08:45 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.78 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,24 +2,31 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int cnt=0;        
+        // int cnt=0;        
+        // for(int i=0; i<arr.length; i++) {
+        //     if(i!=arr.length-1 && arr[i]==arr[i+1]) {
+        //         continue;
+        //     } else {
+        //         cnt++;
+        //     }
+        // }
+        // int[] answer = new int[cnt];
+        // cnt=0;
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<arr.length; i++) {
             if(i!=arr.length-1 && arr[i]==arr[i+1]) {
                 continue;
             } else {
-                cnt++;
+                list.add(arr[i]);
+                // answer[cnt] = arr[i];
+                // cnt++;
             }
         }
-        int[] answer = new int[cnt];
-        cnt=0;
-        for(int i=0; i<arr.length; i++) {
-            if(i!=arr.length-1 && arr[i]==arr[i+1]) {
-                continue;
-            } else {
-                answer[cnt] = arr[i];
-                cnt++;
-            }
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
+
         return answer;
     }
 }
