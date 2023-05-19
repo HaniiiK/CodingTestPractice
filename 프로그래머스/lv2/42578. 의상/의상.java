c74b00976885db59b5060b892e246b7788ee593b
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-05-19 16:58:00 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<clothes.length; i++) {
+            if(map.containsKey(clothes[i][1])) {
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);
+            }else {
+                map.put(clothes[i][1], 1);
+            }
+        }
+        
+        for(String s : map.keySet()) {
+            answer *= map.get(s)+1;
+        }
+        
+        return answer-1;
+    }
+}
