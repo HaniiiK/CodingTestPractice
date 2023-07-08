f457c293e655fc64d290fe78557f4d4e09e476b8
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-07-08 20:16:20 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {        
+        int sub = 0;
+        int[] day = new int[progresses.length];
+        for(int i=0; i<progresses.length; i++) {
+            sub = 100;
+            while(true) {
+                sub -= speeds[i];
+                day[i]++;
+                if(sub<=progresses[i]) {
+                    break;
+                }
+            }
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        int tmp = 1;
+        int max = day[0];
+        for(int i=1; i<day.length; i++) {
+            if(max>=day[i]) {
+                tmp++;
+            }else {
+                list.add(tmp);
+                tmp = 1;
+                max = day[i];
+            }
+        }
+        list.add(tmp);        
+
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }        
+        return answer;
+    }
+}
