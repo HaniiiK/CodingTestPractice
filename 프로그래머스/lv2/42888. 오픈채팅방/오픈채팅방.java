ce35970ca5fb3aca6beb017c869fee40e68ba721
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-07-31 23:51:55 +0900

    [level 2] Title: 오픈채팅방, Time: 187.09 ms, Memory: 172 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] record) {
+//        List<String> answerList = new ArrayList<>();
+        Map<String, String> uid = new HashMap<>(); //uid, 닉네임
+        List<String> enterChk = new ArrayList<>(); //"uid Enter", "uid Leave" 이런 식으로 저장
+        
+        for(int i=0; i<record.length; i++) {
+            String[] str = record[i].split(" ");
+            
+            if(str[0].equals("Enter")) {
+                if(!uid.containsKey(str[1])) {
+                    uid.put(str[1], str[2]); //처음 들어온 사람이면 그냥 추가
+                }else {
+                    if(!uid.get(str[1]).equals(str[2])) {
+                        uid.replace(str[1], str[2]);
+                    }
+                }
+                enterChk.add(str[1]+"님이 들어왔습니다.");
+                
+            }else if(str[0].equals("Leave")) {
+                enterChk.add(str[1]+"님이 나갔습니다.");
+                
+            }else if(str[0].equals("Change")) {
+                uid.replace(str[1], str[2]);
+            }
+        }
+        
+        String[] answer = new String[enterChk.size()];
+        for(int i=0; i<enterChk.size(); i++) {
+            String[] sep = enterChk.get(i).split("님");
+            answer[i] = uid.get(sep[0])+"님"+sep[1];
+        }
+                
+        return answer;
+    }
+}
