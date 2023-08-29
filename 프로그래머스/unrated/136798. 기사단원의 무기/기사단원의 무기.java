fe69b48f63a324bfb52d1cd195d50f8de16cd3d3
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-08-29 23:02:19 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.96 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
+        
         int[] arr = new int[number]; //약수 담기
         for(int i=1; i<=number; i++) {
             for(int j=1; j<=Math.sqrt(i); j++) {
