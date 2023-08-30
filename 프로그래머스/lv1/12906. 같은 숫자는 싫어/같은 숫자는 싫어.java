8ca3a0eb07665368ef29660cbefe996df6cb0c65
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-08-30 13:29:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.40 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,29 +2,20 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        // int cnt=0;        
-        // for(int i=0; i<arr.length; i++) {
-        //     if(i!=arr.length-1 && arr[i]==arr[i+1]) {
-        //         continue;
-        //     } else {
-        //         cnt++;
-        //     }
-        // }
-        // int[] answer = new int[cnt];
-        // cnt=0;
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<arr.length; i++) {
-            if(i!=arr.length-1 && arr[i]==arr[i+1]) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(arr[0]);
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i]==stack.peek()) {
                 continue;
-            } else {
-                list.add(arr[i]);
-                // answer[cnt] = arr[i];
-                // cnt++;
+            }else {
+                stack.add(arr[i]);
             }
         }
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
+        
+        int len = stack.size();
+        int[] answer = new int[len];
+        for(int i=len-1; i>=0; i--) {
+            answer[i] = stack.pop();
         }
 
         return answer;
