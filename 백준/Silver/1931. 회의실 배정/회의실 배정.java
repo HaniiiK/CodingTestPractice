195ec6324e1c22cd6c569ef3356b81a4a8951c1f
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-09-04 12:04:08 +0900

    [Silver I] Title: 회의실 배정, Time: 640 ms, Memory: 55036 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        Map<Long, Long> map = new HashMap<>();
+
+        int answer = 0;
+        long start=0, end=0;
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            start = Long.parseLong(st.nextToken());
+            end = Long.parseLong(st.nextToken());
+            if(start==end) { //바로 카운팅해줌
+                answer++;
+            }
+
+            if(map.containsKey(start)) { //시작 시간이 같은 게 들어오면
+                long value = map.get(start);
+                if((start!=end && value>end) || start==value) { //더 짧은 시간으로 저장하되, 시작시간이랑 종료시간 같은건 무시함
+                    map.replace(start, end);
+                }
+            }else {
+                map.put(start, end);
+            }
+        }
+
+        PriorityQueue<long[]> queue = new PriorityQueue<>(new Comparator<long[]>() {
+            @Override
+            public int compare(long[] o1, long[] o2) {
+                if(o1[0] > o2[0]) {
+                    return 1;
+                }else {
+                    return -1;
+                }
+            }
+        });
+        Set<Map.Entry<Long, Long>> set = map.entrySet();
+        Iterator<Map.Entry<Long, Long>> iter = set.iterator();
+        while(iter.hasNext()) {
+            Map.Entry<Long, Long> next = iter.next();
+            queue.offer(new long[]{next.getKey(), next.getValue()});
+        }
+
+        long[] pre = queue.poll();
+        long saveEnd = pre[1];
+        if(pre[0]!=pre[1]) {
+            answer++;
+        }
+
+        while (!queue.isEmpty()) {
+            long[] poll = queue.poll();
+            if(pre[1] > poll[1]) { //0 6, 1 4같은 경우
+                if(poll[0] == poll[1]) { //0 6, 1 1같은 경우
+                    answer--;
+                }
+                saveEnd = poll[1]; //1 4를 한거로 치는거임
+                pre[0] = poll[0];
+                pre[1] = poll[1];
+            }else if(pre[1] <= poll[1] && saveEnd <= poll[0]) { //0 6, 6 7같은 경우
+                saveEnd = poll[1];
+                if(poll[0]!=poll[1]) {
+                    answer++;
+                }
+                pre[0] = poll[0];
+                pre[1] = poll[1];
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
