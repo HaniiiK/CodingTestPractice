4ec36d49a739cc481525e936f18d7721dbcc8898
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-09-06 00:06:54 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 348.17 ms, Memory: 398 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -9,24 +9,21 @@ class Solution {
                 String binary = Long.toBinaryString(numbers[i]);
                 String newBinary = "";
                 binary = "0".concat(binary);
-//                System.out.println("binary="+binary);
+                
                 int j;
                 for(j=binary.length()-1; j>=0; j--) {
                     if(binary.charAt(j)=='0') {
                         newBinary = newBinary.substring(1);
                         newBinary = "10".concat(newBinary);
                         j--;
-//                        System.out.println(newBinary);
                         break;
                     }else {
                         newBinary = (binary.charAt(j)+"").concat(newBinary);
                     }
-//                    System.out.println(newBinary);
                 }
                 if(j>0) {
                     newBinary = (binary.substring(0,j+1)).concat(newBinary);
                 }
-//                System.out.println(newBinary);
                 answer[i] = Long.parseLong(newBinary, 2);
             }
         }
