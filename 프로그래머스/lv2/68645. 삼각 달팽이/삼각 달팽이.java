4af835c3c56a6551422fd2ea46ecc710d9e6e372
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-09-21 14:08:27 +0900

    [level 2] Title: 삼각 달팽이, Time: 12.79 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] solution(int n) {        
+        /**
+         *  각 행별로 열 생성
+         **/
+        int[][] tmp = new int[n][];
+        for(int i=0; i<n; i++) {
+            tmp[i] = new int[i+1];
+        }
+
+        int[] answer = new int[(int)((1+n)*(n/(double)2))];
+        int num = 1;
+        int x = 0;
+        int y = 0;
+        int maxX = n-1;
+        int minY = 0;
+        
+        while(true) {
+            if(x<maxX && y==minY) {
+                tmp[x++][y] = num++;
+                if(x==maxX) {
+                    minY++;
+                }
+            }else if(x==maxX && y<tmp[x].length-minY) {
+                tmp[x][y++] = num++;
+            }else if(y==tmp[x].length-minY) {
+                tmp[x--][y--] = num++;
+                if(y==minY) {
+                    maxX--;
+                }
+            }
+            
+            if(num>answer.length) {
+                break;
+            }
+        }
+        
+        int idx = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<tmp[i].length; j++) {
+                answer[idx++] = tmp[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
