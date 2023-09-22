c1d4f82d426f158feab2bfc8ad5668515d3ca3ec
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-09-22 16:40:17 +0900

    [level 3] Title: 단어 변환, Time: 0.38 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+    static Queue<Node> queue;
+    static boolean[] visited;
+    static int answer;
+    
+    static class Node {
+        private String word;
+        private int cnt;
+        
+        public Node(String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
+        }
+    } 
+    
+    public int solution(String begin, String target, String[] words) {
+        
+        queue = new LinkedList<>();
+        visited = new boolean[words.length];
+        
+        answer = 0;
+        queue.offer(new Node(begin, 0));
+        bfs(words, target);
+        
+        return answer;
+    }
+    
+    private void bfs(String[] words, String target) {
+        if(queue.isEmpty()) {
+            return;
+        }
+        
+        Node tmp = queue.poll();
+        if(tmp.word.equals(target)) {
+            answer = tmp.cnt;
+            return;
+        }
+        
+        int sameWord;
+        for(int i=0; i<words.length; i++) {
+            if(visited[i]) {
+                continue;
+            }
+
+            sameWord = 0;
+            for(int j=0; j<words[i].length(); j++) {
+                if(tmp.word.charAt(j)==words[i].charAt(j)) {
+                    sameWord++;
+                }
+            }
+            
+            if(sameWord == words[i].length()-1) {
+                visited[i] = true;
+                queue.offer(new Node(words[i], tmp.cnt+1));
+            }            
+        }
+        
+        bfs(words, target);
+    }
+}
