e30743decaa1991243b350f8873569fc7fa461d3
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-10-01 22:17:36 +0900

    [level 3] Title: 야근 지수, Time: 106.38 ms, Memory: 68.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0; i<works.length; i++) {
+            queue.offer(works[i]);
+        }
+
+        while(true) {
+            if(queue.isEmpty()) {
+                break;
+            }
+            
+            int num = queue.poll()-1;
+            if(num>0) {
+                queue.offer(num);
+            }
+
+            if(--n==0) {
+                break;
+            }
+        }
+        
+        while(true) {
+            if(queue.isEmpty()) {
+                break;
+            }
+            
+            int num = queue.poll();
+            answer += num*num;
+        }
+        
+        return answer;
+    }
+}
