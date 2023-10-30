407b8f3a91c0b8aad193f0a6ea0ab8f527269753
Haeun Kim <127297705+HaniiiK@users.noreply.github.com>
2023-10-30 11:50:19 +0900

    [Gold IV] Title: 미로만들기, Time: 136 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -2,20 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, k;
+    static int n;
     static int[][] board;
     static int[][] distance;
-    static Queue<int[]> queue;
+    static boolean[][] visited;
     static int[] moveX = {1, 0, -1, 0};
     static int[] moveY = {0, 1, 0, -1};
+
+    static class Node {
+        int x;
+        int y;
+        int cnt;
+        public Node(int x, int y, int cnt) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
+
         board = new int[n][n];
         distance = new int[n][n];
-        queue = new LinkedList<>();
 
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -30,35 +41,37 @@ public class Main {
             Arrays.fill(distance[i], Integer.MAX_VALUE);
         }
 
-        distance[0][0] = 0;
-        queue.offer(new int[]{0, 0});
-        bfs();
+        visited = new boolean[n][n];
+
+        bfs1();
 
         System.out.println(distance[n-1][n-1]);
     }
 
-    static private void bfs() {
-        if(queue.isEmpty()) {
-            return;
-        }
+    static private void bfs1() {
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cnt, o2.cnt));
 
-        int[] poll = queue.poll();
-        int x = poll[0];
-        int y = poll[1];
+        distance[0][0] = 0;
+        visited[0][0] = true;
+        pq.offer(new Node(0, 0, 0));
 
-        for(int i=0; i<4; i++) {
-            int newX = x+moveX[i];
-            int newY = y+moveY[i];
-            if(newX>=0 && newX<n && newY>=0 && newY<n && distance[x][y] < distance[newX][newY]) {
-                if(board[newX][newY]==1) {
-                    distance[newX][newY] = distance[x][y];
-                }else {
-                    distance[newX][newY] = distance[x][y]+1;
+        while(!pq.isEmpty()) {
+            Node poll = pq.poll();
+            visited[poll.x][poll.y] = true;
+
+            for(int i=0; i<4; i++) {
+                int newX = poll.x + moveX[i];
+                int newY = poll.y + moveY[i];
+                if (newX >= 0 && newX < n && newY >= 0 && newY < n && !visited[newX][newY] && distance[poll.x][poll.y] < distance[newX][newY]) {
+                    if (board[newX][newY] == 1) {
+                        distance[newX][newY] = distance[poll.x][poll.y];
+                    } else {
+                        distance[newX][newY] = distance[poll.x][poll.y] + 1;
+                    }
+                    visited[newX][newY] = true;
+                    pq.offer(new Node(newX, newY, distance[newX][newY]));
                 }
-                queue.offer(new int[]{newX, newY});
             }
         }
-
-        bfs();
     }
 }
